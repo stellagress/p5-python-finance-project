c0e9d661a7f1da7ba4bd42c971cde0a98dd09aca
Stella Mazzillo <stellagress5@gmail.com>
2023-10-03 01:48:41 -0400

    adjusts style of home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,15 +5,18 @@ import "../index.css";
 
 function Home() {
   return (
-    <div className= "image-centered">
-      <img
-            className= "image"
-            src="https://m.foolcdn.com/media/dubs/images/original_imageshttpsg.foolcdn.comeditorialimag.width-880_SGlVtUV.jpg" 
-            alt="Main Home Image" 
+
+    <div className="Home-page">
+      <p className="title">Welcome to SM Investments!</p>
+      <div className= "image-centered">
+        <img
+              className= "image"
+              src="https://m.foolcdn.com/media/dubs/images/original_imageshttpsg.foolcdn.comeditorialimag.width-880_SGlVtUV.jpg" 
+              alt="Main Home Image" 
+              
             
-          
-      />
-      <p>Welcome to the main page!</p>
+        />
+      </div>
     </div>
   );
 }
