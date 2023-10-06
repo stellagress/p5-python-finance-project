435cdb9348a5372179cdb381e9a631793edfafe9
Stella Mazzillo <stellagress5@gmail.com>
2023-10-06 13:44:46 -0400

    saving displaying companies' id in sell page

--- client/src/components/SellStocks.js
+++ client/src/components/SellStocks.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 function SellStocks({ user }) {
-  const [companyInfo, setCompanyInfo] = useState([]);
   const [portfolioStocks, setPortfolioStocks] = useState([]);
+
 
   useEffect(() => {
     const portfolioStocksData = user?.portfolios[0]?.portfolio_stocks;
@@ -11,14 +11,6 @@ function SellStocks({ user }) {
       setPortfolioStocks(portfolioStocksData);
     }
   }, [user]);
-
-  useEffect(() => {
-    const companyData = user?.portfolios[0]?.portfolio_stocks[4]?.stock;
-    if (companyData) {
-      setCompanyInfo(companyData);
-    }
-  }, [user]);
-
 
 
   const handleSellButton = () => {
@@ -30,9 +22,17 @@ function SellStocks({ user }) {
   }
 
 
-  const handleCheckBox= () => {
-    console.log("Box checked")
+  const handleCheckBox = () => {
+    console.log("checked/unchecked")
+    user.portfolios[0].portfolio_stocks.forEach(function(stockItem) {
+      console.log(stockItem.stock.id); 
+    });
   }
+  
+  
+
+  // console.log(user.portfolios[0].portfolio_stocks[4].stock.id)
+  // console.log(user.portfolios[0].portfolio_stocks)
 
 
 
@@ -47,14 +47,13 @@ function SellStocks({ user }) {
             onChange={handleCheckBox}
             />
             <div>
-              <p>Company: {companyInfo.name}</p>
-              <p>Current Dividend Yield: {companyInfo.current_dividend_yield}</p>
-              <p>Market Percentage Variation: {companyInfo.market_percentage_variation}</p>
+              <p>Company: {portfolioStock.stock.name}</p>
+              <p>Current Dividend Yield: {portfolioStock.stock.current_dividend_yield}</p>
+              <p>Market Percentage Variation: {portfolioStock.stock.market_percentage_variation}</p>
             </div>
             <div>
               <p>Shares Quantity: {portfolioStock.shares_quantity}</p>
               <p>Price per Share: {portfolioStock.price_per_share}</p>
-              
               <input
                   type="text"
                   placeholder="Enter quantity"
@@ -62,7 +61,6 @@ function SellStocks({ user }) {
                 />
               <p>Total to receive: </p>
               <button onClick={handleSellButton}>Sell</button>
-
               <p>------------------------------------</p>
             </div>
           </li>
@@ -81,52 +79,47 @@ export default SellStocks;
 
  // console.log(user.portfolios[0].portfolio_stocks[4].stock)
 
+//  console.log(user.portfolios[0].portfolio_stocks)
 
 
-//  import React, { useEffect, useState } from "react";
 
-//  function SellStocks({ user }) {
-//    const [companyInfo, setCompanyInfo] = useState([]);
-//    const [portfolioStocks, setPortfolioStocks] = useState([]);
- 
-//    useEffect(() => {
-//      const portfolioStocksData = user?.portfolios[0]?.portfolio_stocks;
- 
-//      if (portfolioStocksData) {
-//        setPortfolioStocks(portfolioStocksData);
-//      }
-//    }, [user]);
- 
-//    useEffect(() => {
-//      const companyData = user?.portfolios[0]?.portfolio_stocks[4]?.stock;
-//      if (companyData) {
-//        setCompanyInfo(companyData);
-//      }
-//    }, [user]);
- 
-//    return (
-//      <div>
-//        <h4>Sell Stocks Page:</h4>
- 
-//        <ul>
-//          {portfolioStocks.map((portfolioStock, index) => (
-//            <li key={index}>
-//              <div>
-//                <p>Company: {companyInfo.name}</p>
-//                <p>Current Dividend Yield: {companyInfo.current_dividend_yield}</p>
-//                <p>Market Percentage Variation: {companyInfo.market_percentage_variation}</p>
-//              </div>
-//              <div>
-//                <p>Shares Quantity: {portfolioStock.shares_quantity}</p>
-//                <p>Price per Share: {portfolioStock.price_per_share}</p>
-//                <p>------------------------------------</p>
-//              </div>
-//            </li>
-//          ))}
-//        </ul>
-//      </div>
-//    );
-//  }
- 
-//  export default SellStocks;
+// import React, { useEffect, useState } from "react";
+
+// function SellStocks({ user }) {
+//   const [portfolioStocks, setPortfolioStocks] = useState([]);
+
+//   useEffect(() => {
+//     const portfolioStocksData = user?.portfolios[0]?.portfolio_stocks;
+
+//     if (portfolioStocksData) {
+//       setPortfolioStocks(portfolioStocksData);
+//     }
+//   }, [user]);
+
+//   return (
+//     <div>
+//       <h4>Sell Stocks Page:</h4>
+
+//       <ul>
+//         {portfolioStocks.map((portfolioStock, index) => (
+//           <li key={index}>
+//             <div>
+//               <p>Company: {portfolioStock.stock.name}</p>
+//               <p>Current Dividend Yield: {portfolioStock.stock.current_dividend_yield}</p>
+//               <p>Market Percentage Variation: {portfolioStock.stock.market_percentage_variation}</p>
+//             </div>
+//             <div>
+//               <p>Shares Quantity: {portfolioStock.shares_quantity}</p>
+//               <p>Price per Share: {portfolioStock.price_per_share}</p>
+//               <p>------------------------------------</p>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default SellStocks;
+
  
