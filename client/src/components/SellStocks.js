ec0aba23098570eea5b37fc6ca9a7ddd43e6ec9a
Stella Mazzillo <stellagress5@gmail.com>
2023-10-06 11:39:26 -0400

    adds Q and P to sell stocks page

--- client/src/components/SellStocks.js
+++ client/src/components/SellStocks.js
@@ -1,54 +1,75 @@
 import React, { useEffect, useState } from "react";
 
-
 function SellStocks({ user }) {
-    const [stocks, setStocks] = useState([]);
-
-    useEffect(() => {
-        fetch(`http://localhost:5555/sell/${user?.id}`)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(data => {
-            setStocks(data);
-          })
-          .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-          });
-      }, [user?.id]);
+  const [stockData, setStockData] = useState([]);
+  const [portfolioStocks, setPortfolioStocks] = useState([]);
 
 
-      // console.log(user)
-      console.log(user.portfolios[0].portfolio_stocks[2].price_per_share)
-
-      
+  // useEffect(() => {
+  //   // Fetch the stocks data
+  //   fetch(`http://localhost:5555/sell/${user?.id}`)
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setStocks(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("There was a problem with the fetch operation:", error);
+  //     });
+  // }, [user?.id]);
 
 
 
+  useEffect(() => {
+    const portfolioStocksData = user?.portfolios[0]?.portfolio_stocks;
+
+    if (portfolioStocksData) {
+      setPortfolioStocks(portfolioStocksData);
+    }
+  }, [user]);
 
 
-   return (
+
+
+  console.log(user.portfolios[0].portfolio_stocks[4].stock)
+  //name
+  //current 
+
+
+
+
+
+
+
+  return (
     <div>
-        <h4>Sell Stocks Page:</h4>
-        {stocks.map((stock, index) => (
-                <div key={index}>
-                  <p onClick={()=>console.log(stock.id)}>{stock.name}</p>
-                  <p>{stock.current_dividend_yield}</p>
-                  <p>{stock.market_percentage_variation}</p>
-                  <p>------------------------------------</p>
-                </div>
-              ))}
+      <h4>Sell Stocks Page:</h4>
+      {/* {stocks.map((stock, index) => (
+        <div key={index}>
+          <p onClick={() => console.log(stock.id)}>{stock.name}</p>
+          <p>{stock.current_dividend_yield}</p>
+          <p>{stock.market_percentage_variation}</p>
+          <p>------------------------------------</p>
+        </div>
+      ))} */}
 
-      
-      <div>
-      
-    </div>
+        <ul>
+          {portfolioStocks.map((portfolioStock, index) => (
+            <li key={index}>
+              <p>Shares Quantity: {portfolioStock.shares_quantity}</p>
+              <p>Price per Share: {portfolioStock.price_per_share}</p>
+              <p>------------------------------------</p>
+            </li>
+          ))}
+        </ul>
     </div>
   );
 }
+
 export default SellStocks;
 
 
@@ -58,73 +79,95 @@ export default SellStocks;
 
 
 
+
 // import React, { useEffect, useState } from "react";
-// import { useParams, useNavigate } from "react-router-dom";
+
 
 // function SellStocks({ user }) {
-
-//     let nav = useNavigate()
-//     const { portfolioId } = useParams();
 //     const [stocks, setStocks] = useState([]);
-  
+
 //     useEffect(() => {
-//       // Fetch portfolio details if a portfolio ID is present
-//       if (portfolioId) {
-//         fetch(`http://localhost:5555/sell/${portfolioId}`)
-//           .then((res) => res.json())
-//           .then((data) => setStocks(data))
-//           .catch((error) => console.error(error));
-//       }
-//     }, [portfolioId]);
-  
-//     let portfolioContent;
-  
-//     if (portfolioId) {
-//       // If a portfolio ID is present, render portfolio details
-//       const stocksJsx = stocks.map((stock, index) => (
-//         <div key={index}>
-//           <p>{stock.name}</p>
-//           <p>{stock.current_dividend_yield}</p>
-//           <p>{stock.market_percentage_variation}</p>
-//           <p>-------------------------</p>
-//         </div>
-//       ));
-  
-//       portfolioContent = (
-//         <div>
-//           {stocksJsx}
-//         </div>
-//       );
-//     } else {
-//       // Otherwise, render the list of portfolios
-//       const handleClick = (portfolioId) => {
-//         nav(`/sell/${portfolioId}`)
-//       };
-  
-//       const portfolio = user?.portfolios.map((p) => (
-//         <div key={p.id} onClick={() => handleClick(p.id)}>
-//           <p>Portfolio ID: {p.id}</p>
-//         </div>
-//       ));
-  
-//       portfolioContent = (
-//         <div>
-//           <h5>Your Stocks:</h5>
-//           <h6>{portfolio}</h6>
-//         </div>
-//       );
-//     }
+//         fetch(`http://localhost:5555/sell/${user?.id}`)
+//           .then(response => {
+//             if (!response.ok) {
+//               throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//           })
+//           .then(data => {
+//             setStocks(data);
+//           })
+//           .catch(error => {
+//             console.error('There was a problem with the fetch operation:', error);
+//           });
+//       }, [user?.id]);
+
+
+//       // console.log(user)
+//       // console.log(user.portfolios[0].portfolio_stocks[3].price_per_share)
+
+      
 
 
 
 
-//   return (
+
+//    return (
 //     <div>
-//       <h4>Sell Stocks Page:</h4>
+//         <h4>Sell Stocks Page:</h4>
+//         {stocks.map((stock, index) => (
+//                 <div key={index}>
+//                   <p onClick={()=>console.log(stock.id)}>{stock.name}</p>
+//                   <p>{stock.current_dividend_yield}</p>
+//                   <p>{stock.market_percentage_variation}</p>
+//                   <p>------------------------------------</p>
+//                 </div>
+//               ))}
+
+      
 //       <div>
-//       {portfolioContent}
+      
 //     </div>
 //     </div>
 //   );
 // }
 // export default SellStocks;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// returns the other piece (broken)
+// return (
+//   <div>
+//     <h4>Sell Stocks Page:</h4>
+//     {stocks.map((stock, index) => (
+//       <div key={index}>
+//         <p onClick={() => console.log(stock.id)}>{stock.name}</p>
+//         <p>{stock.current_dividend_yield}</p>
+//         <p>{stock.market_percentage_variation}</p>
+//         <p>------------------------------------</p>
+//       </div>
+//     ))}
+
+//     <div>
+//       {/* Map and display portfolio_stocks from the first portfolio */}
+//       {user.portfolios[0].portfolio_stocks.map((portfolioStock, index) => (
+//         <div key={index}>
+//           <p>Stock ID: {portfolioStock.id}</p>
+//           <p>Shares Quantity: {portfolioStock.shares_quantity}</p>
+//           <p>Price per Share: {portfolioStock.price_per_share}</p>
+//           <p>------------------------------------</p>
+//         </div>
+//       ))}
+//     </div>
+//   </div>
+// );
